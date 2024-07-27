525f4bcad0a110eb13333877bc43e4e05d1617f2
Haocheng Su <haocsu@utu.fi>
2024-07-27 22:34:56 +0300

    Complete 4.19

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,7 +5,8 @@ require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
-
+const loginRouter = require('./controllers/login')
+app.use(express.json())
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -25,10 +26,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.static('dist'))
-app.use(express.json())
+
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.requestLogger)
 app.use(middleware.unknownEndpoint)
--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -8,11 +9,22 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const users = await User.find({})
-    const randomUser = users[Math.floor(Math.random() * users.length)]
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     if (!body.title || !body.url) {
         return response.status(400).json({body})
@@ -20,15 +32,15 @@ blogsRouter.post('/', async (request, response) => {
 
     const blog = new Blog({
         title: body.title,
-        author: body.author || randomUser.name,
+        author: body.author || user.name,
         url: body.url,
         likes: body.likes,
-        user: randomUser._id
+        user: user._id
     })
 
     const savedBlog = await blog.save()
-    randomUser.blogs = randomUser.blogs.concat(savedBlog._id)
-    await randomUser.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
 
     response.status(201).json(savedBlog)
 })
