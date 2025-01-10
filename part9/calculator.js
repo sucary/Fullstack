105ccdaf86de0071a2901c560c53f5935571d1dd
Haocheng Su <haocsu@utu.fi>
2025-01-10 03:48:30 +0200

    Initialize part 9.1 with examples

---
+++ part9/calculator.js
@@ -0,0 +1,22 @@
+var calculator = function (a, b, op) {
+    switch (op) {
+        case 'multiply': return a * b;
+        case 'add': return a + b;
+        case 'divide':
+            if (b === 0)
+                throw new Error('Can\'t divide by 0!');
+            return a / b;
+        default:
+            throw new Error('Operation is not multiply, add or divide!');
+    }
+};
+try {
+    console.log(calculator(1, 5, 'divide'));
+}
+catch (error) {
+    var errorMessage = 'Something went wrong: ';
+    if (error instanceof Error) {
+        errorMessage += error.message;
+    }
+    console.log(errorMessage);
+}
